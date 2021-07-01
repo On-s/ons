61875b29c92de2044aceb007cd168b6d0c3e8d63
ons <https://github.com/On-s/ons>
2021-07-01 15:50:44 +0900

    0701

--- js/src/jq_019_gallery_2.js
+++ js/src/jq_019_gallery_2.js
@@ -1,71 +1,107 @@
 //jQuery ------------------------------
 // flie name : jq_019_gallery_2
 //-------------------------------------
+// 
+
+// 상품생성 이미지, 이름 , 가격 , 설명 
+// ul/li 생성
+// 마우스이벤트 클릭, 포커스
+// 큰이미지 클릭시 팝업창 띄우기 (끄기)
+// 다음버튼 클릭시 ul 안에 li 이동
+// 160줄 하다가 멈춤
 (function ($) {
 
   var undrawImgList = [{
-      'ckImg': 'ck1.png',
-      'ckname': '골드킹',
-      'ckprice': '1000000',
-      'ckInfo': '단짠단짠 맛있는 골드킹',
-      'ckPopupLink': 'http://naver.com'
+      "ckImg": "ck1.png",
+      "ckname": "골드킹",
+      "ckprice": "1000000",
+      "ckInfo": "단짠단짠 맛있는 골드킹",
+      "ckPopupLink": "http://naver.com"
     },
     {
-      'ckImg': 'ck2.png',
-      'ckname': '뿌링맵소킹',
-      'ckprice': '2000000',
-      'ckInfo': '맛나게 매운 뿌링 맵소킹',
-      'ckPopupLink': 'http://google.com'
+      "ckImg": "ck2.png",
+      "ckname": "뿌링맵소킹",
+      "ckprice": "2000000",
+      "ckInfo": "맛나게 매운 뿌링 맵소킹",
+      "ckPopupLink": "http://google.com"
     },
     {
-      'ckImg': 'ck3.png',
-      'ckname': '하바네로 포테킹',
-      'ckprice': '3000000',
-      'ckInfo': '난 포테킹은 느끼하더라',
-      'ckPopupLink': 'http://daum.net'
+      "ckImg": "ck3.png",
+      "ckname": "하바네로 포테킹",
+      "ckprice": "3000000",
+      "ckInfo": "난 포테킹은 느끼하더라",
+      "ckPopupLink": "http://daum.net"
     },
     {
-      'ckImg': 'ck4.png',
-      'ckname': '포테킹 콤보',
-      'ckprice': '4000000',
-      'ckInfo': '포테킹은 내취향아님',
-      'ckPopupLink': '#'
+      "ckImg": "ck4.png",
+      "ckname": "포테킹 콤보",
+      "ckprice": "4000000",
+      "ckInfo": "포테킹은 내취향아님",
+      "ckPopupLink": "#"
     },
     {
-      'ckImg': 'ck4.png',
-      'ckname': '포테킹 콤보',
-      'ckprice': '4000000',
-      'ckInfo': '포테킹은 내취향아님',
-      'ckPopupLink': '#'
+      "ckImg": "ck5.png",
+      "ckname": "뿌링클",
+      "ckprice": "4500000",
+      "ckInfo": "뿌링뿌링 뿌리링클",
+      "ckPopupLink": "#"
     },
     {
-      'ckImg': 'ck4.png',
-      'ckname': '포테킹 콤보',
-      'ckprice': '4000000',
-      'ckInfo': '포테킹은 내취향아님',
-      'ckPopupLink': '#'
+      "ckImg": "ck6.png",
+      "ckname": "붐바스틱",
+      "ckprice": "4000000",
+      "ckInfo": "붐바스틱 붐바스틱 예에~",
+      "ckPopupLink": "#"
     },
     {
-      'ckImg': 'ck4.png',
-      'ckname': '포테킹 콤보',
-      'ckprice': '4000000',
-      'ckInfo': '포테킹은 내취향아님',
-      'ckPopupLink': '#'
+      "ckImg": "ck7.png",
+      "ckname": "맛초킹 콤보",
+      "ckprice": "4000000",
+      "ckInfo": "매콤짭짤! 밥과 함께 먹기에 딱 좋은 완벽조합",
+      "ckPopupLink": "#"
     },
     {
-      'ckImg': 'ck4.png',
-      'ckname': '포테킹 콤보',
-      'ckprice': '4000000',
-      'ckInfo': '포테킹은 내취향아님',
-      'ckPopupLink': '#'
+      "ckImg": "ck8.png",
+      "ckname": "양념 맵소킹",
+      "ckprice": "2000000",
+      "ckInfo": "맵긴함 ",
+      "ckPopupLink": "#"
+    },
+    {
+      "ckImg": "ck9.png",
+      "ckname": "핫 후라이드",
+      "ckprice": "4000000",
+      "ckInfo": "먹고 배아플수있음",
+      "ckPopupLink": "#"
+    },
+    {
+      "ckImg": "ck10.png",
+      "ckname": "펌치킨",
+      "ckprice": "4000000",
+      "ckInfo": "펌핀펌핀",
+      "ckPopupLink": "#"
+    },
+    {
+      "ckImg": "ck11.png",
+      "ckname": "맵스터 순살",
+      "ckprice": "7000000",
+      "ckInfo": "맵~~맵~",
+      "ckPopupLink": "#"
     }
   ]
+  // 상품 개수
   var wantLiNum = undrawImgList.length;
+
   var part = $('.part');
+  // 큰이미지
   var main_img = part.find('.main_img');
+  // 상품명 구역
   var mainP = main_img.find('.product');
+  // 상품 가격 구역
   var mainPrice = main_img.find('.pirce');
+  // 상품정보 구역
   var mainInfo = main_img.find('.info');
+  // 상품 링크 구역
   var minImg = main_img.find('.img').find('a');
   var sub_img = part.find('.sub_img');
   sub_img.append('<ul></ul>');
@@ -74,15 +110,17 @@
   var subUl = sub_img.find('ul');;
   var subLi;
 
+  // 객체에들어있는 치킨정보
   var ckCon = part.find('.ck_con');
   var ckClose = ckCon.find('.ck_close');
   var ckImg = ckCon.find('.ck_img');
   var ckBg = part.find('.ck_bg');
 
+  //이미지 링크
   var undrawImgLink = '../../../res/renewal_bhc/chicken/';
 
 
-
+  //객체길이만큼 li 생성----------------------------------
   var set_li = function (count) {
 
     for (var n = 0; n < count; n++) {
@@ -94,7 +132,10 @@
         'backgroundImage': 'url("' + undrawImgLink + undrawImgList[n].ckImg + '")'
       });
     }
-  }
+    // 마지막 li 요소를 첫번째 li 이전에 삽입
+    var lastLI = subLi.clone().last();
+    subLi.eq(0).before(lastLI);
+  }//----------------------------------------------------
 
   // 원하는 li 갯수대로 만들기
   set_li(wantLiNum);
@@ -109,11 +150,35 @@
     minImg.css({
       'backgroundImage': 'url("' + undrawImgLink + undrawImgList[i].ckImg + '")'
     });
-  }
+  }//--------------------------------------------------
 
 
+  // innerWidth() > padding  값 포함
+  // outerWidth() > border 값 포함
+  // outerWidth(true) > margin 값 포함
+  var getliWidth = subUl.find('li').eq(0).outerWidth(true);
   
+  // ul +1 길이 설정
+  subUl.css({'width' : (getliWidth+(getliWidth*undrawImgList.length))+'px'},
+            {'marginLeft': (getliWidth*1) +'px'} );
+
+  var slideBtn = $('.btn_box');
+  var prior_btn = $('.prior_btn');
+  var later_btn = $('.later_btn');
+  var mv = 0;
+
+  later_btn.on('click',function(e) {
+    e.preventDefault();
+    mv -= 1;
+    subUl.animate({'marginLeft': (getliWidth * mv) + 'px'}, 200 , 'linear');
+    
+  })
   //기초 첫페이지 작성
+  prior_btn.on('click',function (e) {
+    e.preventDefault();
+    mv += 1;
+    subUl.animate({'marginLeft': (getliWidth * mv) + 'px'}, 200 , 'linear');
+  })
   clikcenvet(0);
 
 
@@ -196,6 +261,7 @@
   minImg.on('click', function (e) {
     e.preventDefault();
     ckImg.attr('style', minImg.attr('style'));
+    ckClose.focus();
     ckStatus();
   });
 
@@ -210,12 +276,26 @@
     ckStatus();
   });
 
-  
-  var slider = $('sub_img');
-  var isMouseDown = false;
-  var startX;
-  var scrollLeft;
+  // ======================================================== slider
+  // var scrollX = subUl.scrollLeft();
+  // var btn_box = sub_img.find('.btn_box');
+  // var left_btn = btn_box.find('.l_btn');
+  // var right_btn = btn_box.find('.r_btn');
 
-  
+  // var moveScrollLeft = function() {
+  //   return function () {
+  //     subUl.scrollLeft() = scrollX += 100;
+  //     // subUl.scrollLeft(scrollX + 100);
+  //   }
+  // }
+
+  // var moveScrollRight = function () {
+  //   return function () {
+  //     subUl.scrollLeft() -= 100;
+  //   }
+  // }
+
+  // left_btn.on('click',moveScrollLeft());
+  // right_btn.on('click',moveScrollRight());
 
 })(jQuery);
